2037236c6f86d077c5f7aed3bb8ad259ad58cdc6
Matthias Guth <samurai@gono.info>
2020-09-18 16:29:05 +0200

    parse trs files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'AGSTranslationHelper';
+  private dec = new TextDecoder("ISO-8859-1");
+  title = "AGSTranslationHelper";
+
+  mainFile: File;
+  additionalFiles: File[] = [];
+  translationLines: Map<string, string[]> = new Map();
+
+  constructor() {}
+
+  onSelectMain(event) {
+    this.mainFile = event.addedFiles[0];
+    this.mainFile.arrayBuffer().then((arrayBuffer) => {
+      const decodedString = this.dec.decode(arrayBuffer);
+      const splittedLines = decodedString.split("\n");
+      for (var i = 0; i < splittedLines.length; i++) {
+        const langString = splittedLines[i];
+        if (!langString.startsWith("//")) {
+          this.translationLines.set(langString, [splittedLines[i + 1]]);
+          i++;
+        }
+      }
+    });
+  }
+
+  onSelectAdditional(event) {
+    this.additionalFiles.push(...event.addedFiles);
+    event.addedFiles.forEach((file: File) => {
+      file.arrayBuffer().then((arrayBuffer) => {
+        const decodedString = this.dec.decode(arrayBuffer);
+        const splittedLines = decodedString.split("\n");
+
+        for (var i = 0; i < splittedLines.length; i++) {
+          // search for the "main string" in the translation map
+          const translationEntry = this.translationLines.get(splittedLines[i]);
+          if (translationEntry) {
+            translationEntry.push(splittedLines[i + 1]);
+            i++;
+          }
+        }
+      });
+    });
+  }
+
+  onRemove(event) {
+    console.log("onRemove", event);
+    /*     this.files.splice(this.files.indexOf(event), 1); */
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxDropzoneModule } from 'ngx-dropzone';
+import { NgObjectPipesModule } from 'ngx-pipes';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    NgxDropzoneModule,
+    NgObjectPipesModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
